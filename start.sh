b4f7c183a1be4bdc07ee7e1b42137aa2caeb9dbc
TGBotz <108778367+TGBotz@users.noreply.github.com>
2022-08-01 01:01:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TGBotz/MissNora /MissNora
+  git clone https://github.com/TGBotz/Miss /Miss
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Miss
