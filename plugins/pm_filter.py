e3240e4183fcb380975dfab4825e4a6fa1aff3b9
TGBotz <108778367+TGBotz@users.noreply.github.com>
2022-08-03 23:03:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -724,7 +724,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"📟 Movie Name : {search}\n👩🏻‍💻 Requested By : {message.from_user.mention}\n🚀 Group : {message.chat.title}"
+        cap = f"<b>📟 Movie Name : {search}</b>\n<b>👩🏻‍💻 Requested By : {message.from_user.mention}</b>\n<b>🚀 Group : {message.chat.title}</b>"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
